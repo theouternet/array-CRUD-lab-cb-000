a363369aadd4fc7f379c168c3e28edb77d5f3239
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-08-31 22:57:51 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,44 @@
 def create_an_empty_array
-  
+  emptyarray = []
 end
 
 def create_an_array
-  
+  fourelements = [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  famous_cats = ["lil' bub", "grumpy cat", "Maru"]
+ 
+famous_cats << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  famous_cats = ["lil' bub", "grumpy cat", "Maru"]
+ 
+famous_cats.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  famous_cats = ["lil' bub", "grumpy cat", "Maru","arrays!"]
+  famous_cats.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+    famous_cats = ["wow", "grumpy cat", "Maru","arrays!"]
+  famous_cats.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    famous_cats = ["lil' bub", "grumpy cat", "am","arrays!"]
+    famous_cats[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+    famous_cats = ["wow", "grumpy cat", "am","arrays!"]
+    famous_cats[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+      famous_cats = ["wow", "grumpy cat", "am","arrays!"]
+    famous_cats[3]
 end
