640379a12d1287962f5f933852241ae13182b26a
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-08-31 22:14:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,26 @@
 def create_an_empty_array
-  
+  emptyarray = []
 end
 
 def create_an_array
-  
+  fourelements = [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  famous_cats = ["lil' bub", "grumpy cat", "Maru"]
+ 
+famous_cats << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  famous_cats = ["lil' bub", "grumpy cat", "Maru"]
+ 
+famous_cats.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["lil' bub", "grumpy cat", "Maru"]
+  array.unshift("arrays!")
 end
 
 def remove_element_from_start_of_array(array)
