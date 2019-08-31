e549b1e3761b7fe5515c479b49e9e7e25caac0d5
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-08-31 22:46:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,7 @@ end
 
 def remove_element_from_end_of_array(array)
   famous_cats = ["lil' bub", "grumpy cat", "Maru","arrays!"]
-  array.pop
+  famous_cats.pop
 end
 
 def remove_element_from_start_of_array(array)
