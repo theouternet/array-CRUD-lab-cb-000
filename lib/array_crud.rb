816ccf876141fb78b20e1488c9601ca8481fada7
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-08-31 22:55:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,6 +30,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
     famous_cats = ["lil' bub", "grumpy cat", "am","arrays!"]
+    famous_cats[2]
 end
 
 def retrieve_first_element_from_array(array)
